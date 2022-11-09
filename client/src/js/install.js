81bd5afd5e5216155b1ff20d2f6d5d69877e82e4
Blessed Adodo <collzbadodo@gmail.com>
2022-11-08 17:58:12 -0800

    polished home work

---
+++ client/src/js/install.js
@@ -0,0 +1,25 @@
+const installBtn = document.getElementById("buttonInstall");
+const textHeader = document.getElementById("textHeader");
+const buttonInstall = document.getElementById("buttonInstall");
+
+// Logic for installing the PWA
+//  Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  // event.preventDefault();
+  installBtn.style.visibility = "visible";
+  textHeader.textContent = "Click the button to install!";
+  
+  // Implement a click event handler on the `installBtn` element
+  installBtn.addEventListener("click", async () => {
+    event.prompt();
+    installBtn.setAttribute("disabled", true);
+    installBtn.textContent = "Installed!";
+  });
+});
+
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  textHeader.textContent = "Successfully installed!";
+  console.log("👍", "appinstalled", event);
+});
